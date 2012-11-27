5a031ed6ccfc6ea996dcc51b0b1ea5b739e93f1d
Toño Silva Portell <jsilva@jcode.cl>
2012-11-27 12:03:54 -0200

    Update lib/doorkeeper/models/active_record/application.rb

--- lib/doorkeeper/models/active_record/application.rb
+++ lib/doorkeeper/models/active_record/application.rb
@@ -2,6 +2,8 @@ module Doorkeeper
   class Application < ActiveRecord::Base
     self.table_name = :oauth_applications
 
+    belongs_to :site
+
     has_many :authorized_tokens, :class_name => "AccessToken", :conditions => { :revoked_at => nil }
     has_many :authorized_applications, :through => :authorized_tokens, :source => :application
 
